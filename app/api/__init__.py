fb6efa87df415d6d7069bf2c9ffabcae30cbccf3
kiidbrian <brnpaintsil@gmail.com>
2017-03-24 13:41:58 +0000

    modified namespace description

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restplus import Namespace, Resource, fields
 
-ns1 = Namespace('users', description='Users related operations')
+ns1 = Namespace('users', description='')
 
 from . import views
